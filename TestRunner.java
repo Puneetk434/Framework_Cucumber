79a72a612a3754ec6378c198e93ead154a155503
Puneetk434 <puneetk434@gmail.com>
2025-10-17 12:50:58 +0530

    Add files via upload

---
+++ TestRunner.java
@@ -0,0 +1,20 @@
+package com.test;
+
+
+import org.junit.runner.RunWith; 
+ 
+import io.cucumber.junit.Cucumber; 
+import io.cucumber.junit.CucumberOptions; 
+ 
+@RunWith(Cucumber.class)     // to run the main project file 
+@CucumberOptions(               // to pass the options further options features file  
+  features =".feature"             // path name,, argument // feature file that can be run 
+  )  
+public class TestRunner { 
+ 
+
+
+
+	}
+
+
